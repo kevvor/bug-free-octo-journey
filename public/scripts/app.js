a6914426c6569f6a89431331bc70bd171b1dcafc
jayl214 <jay.lin@mail.mcgill.ca>
2017-07-23 19:18:48 +0000

    middle of styling

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,21 +8,20 @@ $(document).ready(function() {
 function createMovieItem (obj) {
   let newMovieItem = `
     <div class="rows">
-      <div class="col-md-2>"</div>
-        <div class="col-md-7">
-        <div class="media">
-          <div class="media-left" "media-middle">
-            <a href="#"">
-              <img class="media-object" src="${obj.movie_image}" alt="image">
-            </a>
-          </div>
-          <div class="media-body">
-            <h4 class="media-heading">${obj.movie_name}</h4>
-            <ul>
-              <li class="rating">${obj.movie_rating}</li>
-            </ul>
-          </div>
-        </div>
+      <div class="media">
+
+      <div class="media-left" "media-middle">
+        <a href="#"">
+          <img class="media-object" src="${obj.movie_image}" alt="image">
+        </a>
+      </div>
+      <div class="media-body">
+        <h4 class="media-heading">${obj.movie_name}</h4>
+        <ul>
+          <li class="rating">${obj.movie_rating}</li>
+        </ul>
+      </div>
+
       </div>
     </div>`
   return (newMovieItem);
